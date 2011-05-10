aba484db543d8d8b83affe9f224cd555ed0c2f7b
Julien Dessaux <judessaux@gmail.com>
2011-05-10 23:21:28 +0200

    Implemented bot commands parsing.

--- Hsbot/Message.hs
+++ Hsbot/Message.hs
@@ -1,10 +1,12 @@
 module Hsbot.Message
     ( answerMsg
+    , getCommand
     , readMsg
     , writeMsg
     ) where
 
 import Control.Concurrent
+import qualified Data.List as L
 import Control.Monad.Reader
 import qualified Network.IRC as IRC
 
@@ -23,4 +25,14 @@ answerMsg (IRC.Message (Just (IRC.NickName nick _ _)) _ (channel:_)) msg
     | head channel == '#' = writeMsg . OutgoingMsg $ IRC.Message Nothing "PRIVMSG" [channel, msg]
     | otherwise = writeMsg . OutgoingMsg $ IRC.Message Nothing "PRIVMSG" [nick, msg]
 answerMsg _ _ = return ()
+
+-- | Get the command in the IRC message if there is one
+getCommand :: IRC.Message -> Env IO [String]
+getCommand (IRC.Message _ _ (_:msg:[])) = do
+    currentBotState <- asks envBotState >>= liftIO . readMVar
+    let cmd:stuff = if msg /= "" then words msg else ["",""]
+    if botNickname currentBotState `L.isPrefixOf` cmd
+        then return stuff
+        else return []
+getCommand _ = return []
 
